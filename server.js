49fdf7bace61f69b52c5e33f3e39dfeb6d3d953d
Jared Noble <jsnoble1@gmail.com>
2015-04-06 13:52:53 -0700

    added censor list

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+
+var censored = require("./censoredWords");
 
 app.use(express.static('public'));
 
@@ -10,8 +13,24 @@ io.on('connection', function(socket){
     console.log('a user connected');
 
     socket.on("message", function (msg){
+        var sendMessage = true;
         console.log(msg.message);
-        socket.emit('chat', msg);
+        //break up into individual words
+        var allWords = msg.message.split(' ');
+        //caching length
+        var length = allWords.length;
+        //check for censored words, using for loop for faster performance
+        for(var i = 0; i < length; i++){
+            if(censored[allWords[i]]){
+                sendMessage = false;
+                break;
+            }
+        }
+
+        if(sendMessage){
+            socket.emit('chat', msg);
+        }
+
     });
 
 });
@@ -19,3 +38,4 @@ io.on('connection', function(socket){
 http.listen(port, function(){
     console.log('listening on port:', port);
 });
+
